5f6831c31b07ebb023ab4ef14bcba9516d05dfc9
longinjava <longin1982@gmail.com>
2018-04-15 16:58:55 +0200

    Update tweeterbot.py

--- tweeterbot.py
+++ tweeterbot.py
@@ -1,4 +1,11 @@
 import tweepy
+import time
+import requests
+from bs4 import BeautifulSoup
+
+quote_page = 'https://api.binance.com/api/v1/ticker/price?'
+hdr = {'User-Agent': 'Mozilla/5.0'}
+soup = BeautifulSoup(requests.get(quote_page,hdr).text, 'html.parser')
 
 consumer_key = 'I7dpeQOATsugQ16dX3edQPR3e'
 consumer_secret = '9PA24qlo0SMadnhw7gYGLymJD660KDUgfOqMQaFH54hyBxlrWo'
@@ -9,5 +16,10 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-tweet = "yop"
-api.update_status(status=tweet)
+numer=len(requests.get(quote_page,hdr).text)
+
+api.update_status(status=numer)
+time.sleep(5)
+api.update_status(status=(numer+1))
+time.sleep(5)
+api.update_status(status=(numer+2))
